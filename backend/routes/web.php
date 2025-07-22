3ab3324ad2ae7def15db49172c244123a3b79c95
irolinski <ignacy.rolinski@gmail.com>
2025-07-22 15:31:06 +0200

    feat: add run schedule route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,30 +3,16 @@
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-email-user', function() {
-    try {
-    Mail::mailer('user')->raw('This is a test email for Laravel using Gmail SMTP!', function($message){
-        $message->to(env('ADMIN_MAIL_DESTINATION'))->subject('Test Email');
-    });
-    return 'Test email sent';
-} catch (\Exception $e){
-    Log::error('Mail sending failed: '.$e->getMessage());
-    return 'Failed to send test email. Check logs for details';
-}
-});
-Route::get('/test-email-admin', function() {
-    try {
-    Mail::mailer('admin')->raw('This is a test email for Laravel using Gmail SMTP!', function($message){
-        $message->to(env('ADMIN_MAIL_DESTINATION'))->subject('Test Email');
-    });
-    return 'Test email sent';
-} catch (\Exception $e){
-    Log::error('Mail sending failed: '.$e->getMessage());
-    return 'Failed to send test email. Check logs for details';
-}
+
+Route::get('/run-schedule', function() {
+    abort_unless(request('token') === env('CRON_TOKEN'), 403);
+    Artisan::call('schedule:run');
+        return 'Scheduler executed!';
+
 });
